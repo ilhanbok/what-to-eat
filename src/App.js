d2028bbc9c4ea28c959e2c1766844bcf8a5ee549
Yezhou Cheng <44252987+xiaolinyier@users.noreply.github.com>
2020-04-10 23:55:03 -0500

    encapsulate our App component within a Router

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { BrowserRouter as HashRouter, Route, Switch } from 'react-router-dom';
+import React,{useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-
-// components
-import Header from './components/layout/Header';
-import Initial from './components/Initial'
-import Home from './components/Home';
-import Profile from './components/Profile';
-import Favorite from "./components/Favorite";
-import Login from "./components/login/Login";
-import Signup from "./components/signup/Signup";
-import RestaurantInfo from "./components/RestaurantInfo";
-
-
+import Routes from "./routes";
+import { AppContext } from "./libs/contextLib";
 
 
 // css
 import './css/style.css'
 
-
-class App extends Component {
-  render() {
-    return (
-        <BrowserRouter>
-          { /* including the Title and other components */}
-          <Switch>
-            <Route exact path='/home' component={Home} />
-            <Route exact path='/' component={Initial} />
-            <Route exact path='/profile' component={Profile} />
-            <Route exact path='/favorite' component={Favorite} />
-            <Route exact path='/login' component ={Login}/>
-            <Route exact path='/signup' component ={Signup}/>
-            <Route exact path='/restaurantinfo' component={RestaurantInfo} />
-            {/* <Route exact path='/login' component={Login}/> */}
-          </Switch>
-        </BrowserRouter>
-    );
+function App(){
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  function handleLogout() {
+    userHasAuthenticated(false);
   }
+  return(
+    //Provider shows that all the child components should be able to access what we put in it
+    <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+      <Routes />
+    </AppContext.Provider>
+  );
 }
-
 export default App;
