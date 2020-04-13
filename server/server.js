20364aaaa67485a56fff35730c02bb73589b4915
Alex Yang <lyang298@wisc.edu>
2020-04-13 17:35:34 -0500

    updated RestaurantInfo.js in server folder

--- server/server.js
+++ server/server.js
@@ -24,12 +24,12 @@ mongo.app.post('/rest_info', function(req, res) {
           console.error(err);
         } else {
           if (null == comments) {
-            res.send({ info: info, comments: null, average: null });
+            res.send({ info: info, comments: [{username:"Alex",text:"good"},{username:"Ilhan",text:"bad"}], average: '' });
           } else {
             res.send({ info : info, comments: comments[0], average: comments[1] });
           }
         }
-      });
+      }, req.body.business_id);
     }
   }, req.body.business_id); 
 });
