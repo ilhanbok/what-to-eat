fc63437bef7760cfe083c9296ab8555a4a992076
Yewon Lee <ylee578@wisc.edu>
2020-04-25 11:17:39 -0500

    search bar style adjusted

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import '../../css/style.css';
+
 //const searchTerm = useRef(null);
 const SearchBar = ({ searchTerm, searchTextChange, searchByValue }) => {
   return (
     <div className="search-bar" >
       <input className="search-box" id='textTerm'
-        type="text"
+        type="text" style={{padding:'0em 0em 0em 3%', fontSize:'0.95em'}}
         /*value={searchText}*/
         /*onKeyPress={searchTextChange}
         onChange={searchTextChange}*/
