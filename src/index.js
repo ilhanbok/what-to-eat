2f885a2b5c1fd9fc85784bcbfefab45b69044be7
Yewon Lee <ylee578@wisc.edu>
2020-05-01 17:56:17 -0500

    increase code coverage for config.js

--- src/index.js
+++ src/index.js
@@ -8,22 +8,22 @@ import { BrowserRouter as Router } from 'react-router-dom';
 Amplify.configure({
     Auth: {
       mandatorySignIn: true,
-      region: config.cognito.REGION,
-      userPoolId: config.cognito.USER_POOL_ID,
-      identityPoolId: config.cognito.IDENTITY_POOL_ID,
-      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+      region: config().cognito.REGION,
+      userPoolId: config().cognito.USER_POOL_ID,
+      identityPoolId: config().cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: config().cognito.APP_CLIENT_ID
     },
     Storage: {
-      region: config.s3.REGION,
-      bucket: config.s3.BUCKET,
-      identityPoolId: config.cognito.IDENTITY_POOL_ID
+      region: config().s3.REGION,
+      bucket: config().s3.BUCKET,
+      identityPoolId: config().cognito.IDENTITY_POOL_ID
     },
     API: {
       endpoints: [
         {
           name: "whattoeat",
-          endpoint: config.apiGateway.URL,
-          region: config.apiGateway.REGION
+          endpoint: config().apiGateway.URL,
+          region: config().apiGateway.REGION
         },
       ]
     }
